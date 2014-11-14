373eb53cdd09fb9f32cc1eca8d6d8953fb694692
Rodolfo <jrodolfolima@yahoo.com.br>
2014-11-14 12:50:11 -0200

    arquivo quase pronto, exceto pela impressão do recibo

---
+++ alan_CH2.c
@@ -0,0 +1,360 @@
+/**
+ *
+ * Script para o funcionamento de uma garagem
+ * @authors Alan Mesquita de Sousa & Jorge Rodolfo Lima
+ * @link https://github.com/alanuhu
+ * @version 0.1.3
+ *
+ * Copyright © Alan Mesquita & Jorge Rodolfo Lima- 2014
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * const int TOTAL_PARKING_SPACES | numero total de espaços na garagem
+ * const float INITIAL_PRICE | preço da primeira hora
+ * const float TIME_PRICE | preço das demais horas
+ */
+#define TOTAL_PARKING_SPACES 3
+#define INITIAL_PRICE  20.00 /** Valor da primeira hora */
+#define TIME_PRICE  2.00     /** Valor da fração adicional de 15 min */
+
+/** Declaracao de funcoes
+ * int checkPlate | verifica se a placa fornecida eh valida
+ * int searchCar | busca uma placa no vetor de carros
+ * char parkingOptions | oferece menu retorna opcao
+ * float calcValue | recebe o tempo de permanência em muntos e retorna o valor
+ * void addCar | fluxo de entrada de carro da garagem
+ * void listCar | apresenta uma lista das placas do carros na garagem
+ * void removeCar | fluxo de saida de carro da garagem
+ * void dateFileName | cria o arquivo com o nome correspondendo aa data (formato
+                       YYYY-MM-DD.txt e inclui cabecalho inicial
+*/
+int checkPlate (char texto[8]);
+int searchCar(char *carPlate);
+char parkingOptions();
+float calcValue(int mins);
+void addCar();
+void listCar();
+void removeCar();
+void dateFileName();
+
+/**
+ * Global vars
+ * char cars | array de carros
+ * char dates | array de datas
+ * int remainingSpaces | espaços remanecentes
+ * float totalValue | arrecadação total do dia
+ * fileP | apontador pro arquivo de saida
+ */
+char cars[TOTAL_PARKING_SPACES][8],
+     datesString[TOTAL_PARKING_SPACES][50],
+     reportFileName [15],
+     buff[20];
+int dates[TOTAL_PARKING_SPACES],
+    remainingSpaces = TOTAL_PARKING_SPACES;
+float totalValue = 0.00;
+FILE* fileP;
+time_t now;
+
+
+void main()
+{
+  char option;
+  int i;
+
+  for (i=0;i<TOTAL_PARKING_SPACES;i++) { /** "Zera" os vetores a serem utilizados " */
+      cars[i][0]='\0';
+      datesString[i][0]='\0';
+      }
+  dateFileName();                        /** cria e abre arquivo e saída e armazena o nome do arquivo em reportFileName[15] */
+
+  printf("\033[96m***************************************************** \n");
+  printf("**             Bem vindo a nossa garagem           ** \n");
+  printf("*****************************************************\033[0m \n");
+
+  while(1) {
+
+    printf("\n\n-----------------------------------------------------\n");
+    printf("|    Vagas restantes: \033[93m%d\033[0m    |    Total de vagas \033[93m%d\033[0m    |", remainingSpaces, TOTAL_PARKING_SPACES);
+    printf("\n-----------------------------------------------------\n\n");
+    if(remainingSpaces == 0) {
+        printf("\n\n  \033[1;31m NAO HA VAGAS\033[0m\n");
+        }
+    option = parkingOptions();
+
+    switch (option) {
+    case '1' :
+        if(remainingSpaces == 0) {
+          printf("\n\n  \033[1;31m NAO HA VAGAS\033[0m\n");
+          sleep(2);
+          continue;
+          }
+        addCar();
+        break;
+
+    case '2' :
+      if(remainingSpaces == TOTAL_PARKING_SPACES) {
+          printf("\n\n  \033[1;31m Não existem carros na garagem\033[0m\n");
+          sleep(2);
+          continue;
+          }
+      removeCar();
+      break;
+
+    case '3' :
+      printf("\n\n  \033[92m O valor total do dia é: R$ %.2f.\033[0m\n\n\n\n", totalValue);
+      printf ("\n\nDeseja encerrar o dia? (1 = Sim/ 2 = Nao): ");
+      scanf ("%s", &option);
+      if (option=='1') {
+          fprintf (fileP, "\n\t\t\t\tTOTAL DO DIA: R$ %.2f", totalValue);
+          fclose(fileP);
+          printf("\n\n  \033[92m Ate logo!\033[0m\n\n\n\n");
+          sleep(2);
+          exit(EXIT_SUCCESS);
+          }
+      else {
+          parkingOptions();
+          }
+      break;
+
+    case'0' :
+      printf ("\n\tTEM CERTEZA QUE DESEJA SAIR SEM FAZER O FECHAMENTO?");
+      printf ("\n\tTodos os dados serao perdidos!\n\t\t\t(0 = Sim / 2 = Nao): ");
+      scanf ("%s", &option);
+      if (option!='0') {
+          parkingOptions();
+          }
+      else {
+          printf("\n\n  \033[92m Ate logo!\033[0m\n\n\n\n");
+          sleep(2);
+          exit(EXIT_SUCCESS);
+          }
+      break;
+    }
+  }
+ return;
+}
+
+/**
+ *
+ * Função que retorna a opção selecionada
+ * @return int | opção escolhida
+ */
+char parkingOptions() {
+  char output[2];
+  printf("\n\n  Escolha a função\n\n  • 1 - Entrada\n  • 2 - Saída\n  • 3 - Fechamento do Dia\n  • 0 - Encerrar :  ");
+  scanf ("%s", &output);
+  output[1]='\0';  /** Evita alguns problemas caso o usuario digite varios caracteres ao inves de um soh*/
+  return output[0];
+}
+
+/**
+ *
+ * Função que adiciona carro
+ * @return void
+ */
+void addCar() {
+  char carPlate [8],
+       confirm;
+  int i;
+
+  printf("\n  Carro entrando\n\tDigite a placa:  ");
+  scanf("%s", carPlate);
+  for (i=0;i<3;i++) {
+      if (carPlate[i]>=97 && carPlate[i]<=122) {
+          carPlate[i] -= 32; /** Passa as letras da placa do carro para maiusculas*/
+          }
+      }
+  printf("\n  Placa: %s. Confirma? (1 para sim / 2 para nao): ", carPlate);
+  scanf("%s", &confirm);
+  if (confirm != '1') {
+      return;
+      }
+  else {
+      if (searchCar(carPlate)!= -1)
+          {
+          printf ("\n\tPLACA INVALIDA - CARRO JÁ INSERIDO. (Tecle: 1 para corrigir / 2 para voltar): ");
+          scanf ("%s", &confirm);
+          if (confirm != '1') {
+              return;
+              }
+          else{
+              addCar();
+              return;
+              }
+          }
+      if (checkPlate(carPlate)!=0)
+          {
+          printf ("\n\tPLACA INVALIDA. (Tecle: 1 para corrigir / 2 para voltar): ");
+          scanf ("%s", &confirm);
+          if (confirm != '1') {
+              return;
+              }
+          else {
+              addCar();
+              return;
+              }
+          }
+    for( i = 0; i < TOTAL_PARKING_SPACES; i++ ) {
+      if( strlen(cars[i]) == 0 ) {
+        strcpy(cars[i], carPlate);
+        i = TOTAL_PARKING_SPACES;
+      }
+    }
+
+    time(&now);
+    dates[searchCar(carPlate)] = now;
+
+    remainingSpaces--;
+
+    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+    printf("\n\n\t\033[92mEntrada:\n\tCarro placa: %s\n\tData/Hora %s.\033[0m\n", carPlate, buff);
+    sleep(2);
+  }
+}
+
+void removeCar() {
+  char carPlate [8];
+  int confirm,
+      carFoundIndex,
+      i;
+  time_t now;
+  char buff[20];
+  int minutes;
+  float hoursValue;
+
+  printf("\n  Carro saindo\n\tDigite a placa:  ");
+  scanf("%s", carPlate);
+  for (i=0;i<3;i++) {
+      if (carPlate[i]>=97 && carPlate[i]<=122) {
+          carPlate[i] -= 32; /** Passa as letras da placa do carro para maiusculas*/
+          }
+      }
+  carPlate[7] = '\0';                                                            /** encerra a string carPlate na 8a. posição*/
+  printf("\n  Placa: %s. Confirma? (1 para sim / 2 para nao): ", carPlate);
+  scanf("%d", &confirm);
+
+  if(confirm == 1) {
+    carFoundIndex = searchCar(carPlate);
+    if(carFoundIndex == -1) {
+      printf("\n\n  \033[1;31m Nenhum carro encontrado com a placa: %s\033[0m\n", carPlate);
+      sleep(1);
+      listCar();
+      sleep(2);
+      removeCar();
+    } else {
+      time(&now);
+      strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+      minutes = (difftime(now, dates[carFoundIndex]))/60;
+      if (minutes<=5) {                                      /**tolerância de 5 minutos sem cobrar */
+          hoursValue = 0;
+          }
+      else {
+          hoursValue = calcValue(minutes);
+          }
+      printf("\n\033[92m\n\tSaída:\n\tCarro placa: %s \n\tData/Hora %s \n\tPermanencia: %dh %dmins.\n\tValor: R$ %.2f.\033[0m\n",
+          carPlate, buff, minutes/60, minutes%60, hoursValue);
+      totalValue += hoursValue;
+      fprintf (fileP,"%s\t%s\t%dh %dmin\tR$ %5.2f\tR$ %5.2f\n",carPlate,buff,minutes/60, minutes%60,hoursValue,totalValue);
+      strcpy(cars[carFoundIndex], "");
+      dates[carFoundIndex] = 0;
+      remainingSpaces++;
+      sleep(2);
+    }
+  }
+  return;
+}
+
+void listCar() {
+  int i;
+
+  printf("\n\t---------------------------------\n");
+  printf("\t|  \033[95mLista de placas disponiveis\033[0m  |\n");
+  printf("\t---------------------------------\n");
+  printf("\t|    \033[93mplaca\033[0m   |  \033[93mhora de entrada\033[0m |\n");
+  printf("\t---------------------------------\n");
+  for (i=0;i<TOTAL_PARKING_SPACES;i++) {
+    if (strlen(cars[i]) != 0 ) {
+      printf("\t|  %8s  |    %d    |\n", cars[i], dates[i]);
+      printf("\t---------------------------------\n");
+    }
+  }
+ return;
+}
+
+/**
+ *
+ * Procura pela placa no @array cars
+ *
+ * @params char | placa do carro
+ * @return int | indice da placa no array
+ */
+int searchCar(char *carPlate) {
+  int output = -1, i;
+  for( i = 0; i < TOTAL_PARKING_SPACES; i++ ) {
+    if( strcmp(cars[i], carPlate) == 0 ) {
+      output = i;
+    }
+  }
+  return output;
+}
+
+float calcValue(int mins) {
+ int fractions; /** fracoes de 15 minutos apos uma hora*/
+ float finalValue;
+ if (mins<=60) finalValue = INITIAL_PRICE;
+ else
+   {
+   if (mins%15==0) fractions = (mins-60)/15;
+   else fractions = 1 + ((mins-60)/15);
+   finalValue = INITIAL_PRICE + (TIME_PRICE*fractions);
+   }
+return (finalValue);
+}
+
+/**
+ *
+ * Verifica se a placa fornecida eh uma placa valida
+ * (três letras seguidas de quatro algarismos)
+ * @var chk | recebe valor 0 se a placa eh valida ou um valor maior se for invalida
+ * @var ii | indices de posicao dos caracteres no texto */
+
+int checkPlate (char texto[8])
+ {
+ int chk=0, ii;
+
+ for (ii=0;ii<3;ii++) if ((texto[ii]<65)||(texto[ii]>90)) chk+=1;
+ for (ii=3;ii<6;ii++) if ((texto[ii]<48)||(texto[ii]>57)) chk+=1;
+ if (texto[7]!='\0') chk+=1;
+ return (chk);
+ }
+
+ /**
+ *
+ * cria o nome do arquivo de saida com o padrão: YYYY-MM-DD.csv
+ * escreve o cabecalho do arquivo, com data e hora de inicio do sistema
+ */
+
+void dateFileName () {
+
+int i;
+time(&now);
+strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+  for (i=0;i<10;i++) {
+      reportFileName[i]=buff[i];
+      }
+  strcat (reportFileName, ".txt");
+  reportFileName[15]='\0';
+  fileP = fopen (reportFileName, "w");   /** abre o arquivo de saída */
+  fprintf (fileP, "Sistema aberto em (data e hora): %s\n\n", buff);
+  fprintf (fileP, "Placa\tData e Hora de Saida\tEstadia\tValor Pago\tSubtotal\n", buff);
+  return;
+  }
